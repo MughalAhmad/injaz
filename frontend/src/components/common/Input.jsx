f942a7137e5aace5c07f9eccbe5c0d46d311e5d3
MughalAhmad <ahmadkhurshed311@gmail.com>
2024-12-07 01:53:54 +0500

    add validation in team form

---
+++ frontend/src/components/common/Input.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+const Input = ({
+  feedback,
+  title = "text",
+  color = "bg-transparent",
+  px = "px-3",
+  py = "py-4",
+  size = "text-lg", ...props }) => {
+  return (
+    <>
+      <label className='text-xl text-textPrimary font-medium mb-2'>{title}</label>
+      <input
+        {...props}
+        className={`w-full rounded-lg border ${px} ${py} ${size} border-[#D0D5DD] outline-none text-base placeholder-[#D0D5DD] text-black px-3`}
+        />
+      {feedback && feedback[0] && feedback[1] && (
+        <span className="text-red-500">{feedback[0]}</span>
+      )}
+    </>
+  )
+}
+export default Input;
--- frontend/src/components/team/TeamForm.jsx
+++ frontend/src/components/team/TeamForm.jsx
@@ -1,6 +1,76 @@
 import React from 'react'
+import Input from '../common/Input'
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+// import { createNewUser } from '../../redux/features/adminSlice';
+
+const teamCreateSchema = Yup.object({
+    firstName: Yup.string().required("First Name Required"),
+    lastName: Yup.string().required("Last Name Required"),
+    email: Yup.string().email().required("Enter Valid Email"),
+    phone: Yup.number().required("Phone Number Required"),
+    mobile: Yup.number().required("Mobile Number Required"),
+    address: Yup.string().required("Address Required"),
+    userId: Yup.string().required("Last Name Required"),
+    nationality: Yup.string().required("Nationality Required"),
+    password: Yup.string().min(6).max(25).required("Password Required"),
+    confirmPassword: Yup.string()
+        .required("Confirm Password Required")
+        .oneOf([Yup.ref('password'), null], "Passwords must match"),
+});
+
+
 
 const TeamForm = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { values, errors, touched, handleBlur, handleChange, submitForm } =
+        useFormik({
+            initialValues: {
+                firstName: "",
+                lastName: "",
+                email: "",
+                password: "",
+                confirmPassword: "",
+                userType: "",
+                paymentDate: "",
+            },
+            validationSchema: teamCreateSchema,
+            enableReinitialize: true,
+            onSubmit: async (values) => {
+
+                const body = {
+                    firstName: values.firstName,
+                    lastName: values.lastName,
+                    email: values.email,
+                    password: values.password,
+                    confirmPassword: values.confirmPassword,
+                    userType: values.userType,
+                    paymentDate: values.paymentDate,
+                }
+
+                // dispatch(updateShowBackDropLoader(true));
+                // dispatch(createNewUser(body))
+                //     .then(resp => {
+                //         dispatch(updateShowBackDropLoader(false));
+                //         if (resp && !resp.payload.hasError) {
+                //             sweetToast(false, resp.payload.msg);
+                //             navigate('/admin/users');
+                //         } else {
+                //             sweetNotification(true, resp.payload.msg);
+                //         }
+                //     })
+                //     .catch(error => {
+                //         console.log(error);
+                //         dispatch(updateShowBackDropLoader(false));
+                //         sweetNotification(true, 'Something went wrong');
+                //     })
+            },
+        });
   return (
     <div className='px-3 md:px-10'>
     {/* <p className='font-semibold text-2xl text-textPrimary my-11'>Team</p> */}
@@ -11,22 +81,30 @@ const TeamForm = () => {
     <div className='flex flex-col lg:flex-row px-3 md:px-10'>
     <p className='font-semibold text-xl text-textPrimary w-full lg:w-[33%]'>Name</p>
     <div className={`flex flex-col w-full lg:w-[33%] lg:mr-10`}>
-        <label className='text-xl text-textPrimary font-medium mb-2'>First Name</label>
-        <input placeholder="Enter first name"
-        value=""
-        autoComplete='off'
-        name=""
-        type='text' className='w-full h-16 rounded-lg border border-[#D0D5DD] outline-none text-base placeholder-[#D0D5DD] text-black px-3'/>
-
+    <Input
+                            type="text"
+                            title='First Name'
+                            name="firstName"
+                            placeholder="First Name"
+                            size="lg"
+                            value={values.firstName}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            feedback={[errors.firstName, touched.firstName]}
+                        />
     </div>  
     <div className={`flex flex-col w-full lg:w-[33%]`}>
-        <label className='text-xl text-textPrimary font-medium mb-2'>Last Name</label>
-        <input placeholder="Enter last name"
-        value=""
-        autoComplete='off'
-        name=""
-        type='text' className='w-full h-16 rounded-lg border border-[#D0D5DD] outline-none text-base placeholder-[#D0D5DD] text-black px-3'/>
-
+    <Input
+                            type="text"
+                            title='Last Name'
+                            name="lastName"
+                            placeholder="Last Name"
+                            size="lg"
+                            value={values.lastName}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            feedback={[errors.lastName, touched.lastName]}
+                        />
     </div> 
   
       </div>
@@ -34,13 +112,17 @@ const TeamForm = () => {
     <div className='flex flex-col lg:flex-row px-3 md:px-10'>
     <p className='font-semibold text-xl text-textPrimary w-full lg:w-[33%]'>Email</p>
     <div className={`flex flex-col w-full lg:w-[33%]`}>
-        <label className='text-xl text-textPrimary font-medium mb-2'>Email</label>
-        <input placeholder="Enter first name"
-        value=""
-        autoComplete='off'
-        name=""
-        type='text' className='w-full h-16 rounded-lg border border-[#D0D5DD] outline-none text-base placeholder-[#D0D5DD] text-black px-3'/>
-
+    <Input
+                            type="email"
+                            title='Email'
+                            name="email"
+                            placeholder="Enter Email"
+                            size="lg"
+                            value={values.email}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            feedback={[errors.email, touched.email]}
+                        />
     </div>  
       </div>
       {/* Phone */}
@@ -70,22 +152,30 @@ const TeamForm = () => {
        <div className='flex flex-col lg:flex-row px-3 md:px-10'>
     <p className='font-semibold text-xl text-textPrimary w-full lg:w-[33%]'>ID</p>
     <div className={`flex flex-col w-full lg:w-[33%] lg:mr-10`}>
-        <label className='text-xl text-textPrimary font-medium mb-2'>User ID</label>
-        <input placeholder="Enter first name"
-        value=""
-        autoComplete='off'
-        name=""
-        type='text' className='w-full h-16 rounded-lg border border-[#D0D5DD] outline-none text-base placeholder-[#D0D5DD] text-black px-3'/>
-
+    <Input
+                            type="text"
+                            title='User ID'
+                            name="userId"
+                            placeholder="Enter User ID"
+                            size="lg"
+                            value={values.userId}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            feedback={[errors.userId, touched.userId]}
+                        />
     </div>  
     <div className={`flex flex-col w-full lg:w-[33%]`}>
-        <label className='text-xl text-textPrimary font-medium mb-2'>Nationality</label>
-        <input placeholder="Enter last name"
-        value=""
-        autoComplete='off'
-        name=""
-        type='text' className='w-full h-16 rounded-lg border border-[#D0D5DD] outline-none text-base placeholder-[#D0D5DD] text-black px-3'/>
-
+    <Input
+                            type="text"
+                            title='Nationality'
+                            name="nationality"
+                            placeholder="Enter Nationality"
+                            size="lg"
+                            value={values.nationality}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            feedback={[errors.nationality, touched.nationality]}
+                        />
     </div> 
   
       </div>
@@ -93,35 +183,47 @@ const TeamForm = () => {
        <div className='flex flex-col lg:flex-row px-3 md:px-10'>
     <p className='font-semibold text-xl text-textPrimary w-full lg:w-[33%]'>Address</p>
     <div className={`flex flex-col w-full lg:w-[33%]`}>
-        <label className='text-xl text-textPrimary font-medium mb-2'>Address</label>
-        <input placeholder="Enter first name"
-        value=""
-        autoComplete='off'
-        name=""
-        type='text' className='w-full h-16 rounded-lg border border-[#D0D5DD] outline-none text-base placeholder-[#D0D5DD] text-black px-3'/>
-
+    <Input
+                            type="text"
+                            title='Address'
+                            name="address"
+                            placeholder="Enter Address"
+                            size="lg"
+                            value={values.address}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            feedback={[errors.address, touched.address]}
+                        />
     </div>  
       </div>
        {/* Password */}
        <div className='flex flex-col lg:flex-row px-3 md:px-10'>
     <p className='font-semibold text-v text-textPrimary w-full lg:w-[33%]'>Password</p>
     <div className={`flex flex-col w-full lg:w-[33%] lg:mr-10`}>
-        <label className='text-xl text-textPrimary font-medium mb-2'>Password</label>
-        <input placeholder="Enter first name"
-        value=""
-        autoComplete='off'
-        name=""
-        type='text' className='w-full h-16 rounded-lg border border-[#D0D5DD] outline-none text-base placeholder-[#D0D5DD] text-black px-3'/>
-
+    <Input
+                            type="text"
+                            title='Password'
+                            name="password"
+                            placeholder="Enter Password"
+                            size="lg"
+                            value={values.password}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            feedback={[errors.password, touched.password]}
+                        />
     </div>  
     <div className={`flex flex-col w-full lg:w-[33%]`}>
-        <label className='text-xl text-textPrimary font-medium mb-2'>Confirm Password</label>
-        <input placeholder="Enter last name"
-        value=""
-        autoComplete='off'
-        name=""
-        type='text' className='w-full h-16 rounded-lg border border-[#D0D5DD] outline-none text-base placeholder-[#D0D5DD] text-black px-3'/>
-
+        <Input
+                            type="text"
+                            title='Confirm Password'
+                            name="confirmPassword"
+                            placeholder="Enter Confirm Password"
+                            size="lg"
+                            value={values.confirmPassword}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            feedback={[errors.confirmPassword, touched.confirmPassword]}
+                        />
     </div> 
   
       </div>
